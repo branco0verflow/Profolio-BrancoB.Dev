94ce0a5da69e27a3e2aae6d7ff9c729e00a25f36
branco0verflow <brann455@gmail.com>
2025-08-25 11:20:11 -0300

    cambio de nombre de archivos para deployment

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,8 @@ import { MotionSection } from "@/components/Section";
 import ProjectCard from "@/components/ProjectCard";
 import personal from "@/data/personal.json";
 import projects from "@/data/projects.json";
-import TechShowcase from "@/components/TechShowcase";
+import TechShowcase from "../components/TechShowcase";
+
 
 export default function Home() {
   return (
